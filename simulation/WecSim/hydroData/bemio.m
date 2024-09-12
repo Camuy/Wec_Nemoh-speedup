182098e93a15e3bb9983f571b650fe4a11ce3eea
Camuy <98719265+Camuy@users.noreply.github.com>
2024-09-12 21:49:49 +0200

    delate simulation

--- simulation/WecSim/hydroData/bemio.m
+++
@@ -1,8 +0,0 @@
-hydro = struct();
-
-hydro = readNEMOH(hydro, "/Users/riccardo/Documents/Cartella/Progetti/Origami/v0.3/WecSim/input_data");
-hydro = radiationIRF(hydro,60,[],[],[],[]);
-hydro = radiationIRFSS(hydro,[],[]);
-hydro = excitationIRF(hydro,157,[],[],[],[]);
-writeBEMIOH5(hydro)
-plotBEMIO(hydro)
