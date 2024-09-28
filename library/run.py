d07f78b2bb0a5490cd17f05ea39f61fab98fcc33
Camuy <98719265+Camuy@users.noreply.github.com>
2024-09-28 17:34:01 +0200

    Update runner

--- library/run.py
+++ library/run.py
@@ -4,12 +4,13 @@ import shutil
 def init_mesh(path):
     print('Prepering the meshs to be calculated in Nemoh.\n\n')
     
-    os.system(f'meshmagick {path}/Nemoh/float/Nfloat.stl -s 0.001 -i -o {path}/Nemoh/float/Nfloat.nem && meshmagick {path}/Nemoh/spar/Nspar.stl -s 0.001 -i -o {path}/Nemoh/spar/Nspar.nem')
+    os.system(f'meshmagick {path}/Nemoh/float/Nfloat.stl -s 0.001 -i -o {path}/Nemoh/float/Nfloat.nem && meshmagick {path}/Nemoh/spar/Nspar.stl -s 0.001 -i -o {path}/Nemoh/spar/Nspar.nem && meshmagick {path}/Nemoh/Nwec.stl -s 0.001 -i -o {path}/Nemoh/Nwec.nem')
+    os.system(f'meshmagick {path}/WecSim/geometry/float.stl -s 0.001 -i -o {path}/WecSim/geometry/float.stl && meshmagick {path}/WecSim/geometry/spar.stl -s 0.001 -i -o {path}/WecSim/geometry/spar.stl')
 
     # modificare il file Mesh.cal !!!!
     # modificare il file .nem e .dat per entrambi i corpi
     # modificare i file Nemoh.cal per i due corpi e per l'assembly
-    os.system(f'''mesh  {path}/Nemoh/float && mesh  {path}/Nemoh/spar''')
+    os.system(f'''mesh  {path}/Nemoh/float && mesh  {path}/Nemoh/spar && mesh  {path}/Nemoh''')
 
     shutil.copy(f'{path}/Nemoh/float/mesh/Hydrostatics.dat', f'{path}/WecSim/input_data/mesh/Hydrostatics_0.dat')
     shutil.copy(f'{path}/Nemoh/float/mesh/KH.dat', f'{path}/WecSim/input_data/mesh/KH_0.dat')
@@ -17,6 +18,7 @@ def init_mesh(path):
     shutil.copy(f'{path}/Nemoh/spar/mesh/Hydrostatics.dat', f'{path}/WecSim/input_data/mesh/Hydrostatics_1.dat')
     shutil.copy(f'{path}/Nemoh/spar/mesh/KH.dat', f'{path}/WecSim/input_data/mesh/KH_1.dat')
     shutil.copy(f'{path}/Nemoh/spar/Nspar.nem.dat', f'{path}/Nemoh/Nspar.dat')
+    os.rename(f'{path}/Nemoh/Nwec.nem.dat', f'{path}/Nemoh//Nwec.dat')
     return
 
 def init_Nemoh(path):
@@ -44,7 +46,7 @@ def Nemoh(path):
 
     # Solve only the first order equations
     
-    os.system(f'''preProc {path}/Nemoh && hydrosCal {path}/Nemoh && solver {path}/Nemoh && postProc {path}/Nemoh''')
+    os.system(f'''preProc {path}/Nemoh && hydrosCal {path}/Nemoh  && postProc {path}/Nemoh''')
     postProc_Nemoh(path=path)
 
     print('\nNemoh finished\n')
--- sample/WecSim/wecSimInputFile.m
+++ sample/WecSim/wecSimInputFile.m
@@ -2,22 +2,22 @@
 simu = simulationClass();                       % Initialize simulationClass
 simu.simMechanicsFile = 'RM3.slx';              % Simulink Model File
 simu.startTime = 0;                             % Simulation Start Time [s]
-simu.rampTime = 10;                            	% Wave Ramp Time [s]
-simu.endTime = 10;                              % Simulation End Time [s]
-simu.dt = 0.002;                                  % Simulation time-step [s]
+simu.rampTime = 3;                            	% Wave Ramp Time [s]
+simu.endTime = 25;                              % Simulation End Time [s]
+simu.dt = 0.01;                                  % Simulation time-step [s]
 simu.b2b = 1;
 %simu.nonlinearDt = 3*simu.dt;
 
 
 %% Wave Information 
 % % noWaveCIC, no waves with radiation CIC  
-waves = waveClass('noWaveCIC');        % Initialize Wave Class and Specify Type  
+%waves = waveClass('noWaveCIC');        % Initialize Wave Class and Specify Type  
 
 % % Regular Waves
-%waves = waveClass('regular');           % Initialize Wave Class and Specify Type                                
-%waves.height = 0.3;                       % Wave Height [m]
-%waves.period = 4.5;                       % Wave Period [s]
-%waves.direction = 0;
+waves = waveClass('regular');           % Initialize Wave Class and Specify Type                                
+waves.height = 1;                       % Wave Height [m]
+waves.period = 4;                       % Wave Period [s]
+waves.direction = 0;
 
 % % Regular Waves with CIC
 %waves = waveClass('regularCIC');          % Initialize Wave Class and Specify Type                                 
@@ -71,7 +71,7 @@ waves.marker.graphicColor = [1,0,0];
 body(1) = bodyClass('hydroData/input_data.h5');      
     % Create the body(1) Variable, Set Location of Hydrodynamic Data File 
     % and Body Number Within this File.   
-body(1).geometryFile = 'geometry/Float.stl';    % Location of Geomtry File
+body(1).geometryFile = 'geometry/float.stl';    % Location of Geomtry File
 body(1).mass = 5.3026;                   
     % Body Mass. The 'equilibrium' Option Sets it to the Displaced Water 
     % Weight.
@@ -85,12 +85,12 @@ body(1).nonlinearHydro = 2;
 
 % Spar/Plate
 body(2) = bodyClass('hydroData/input_data.h5'); 
-body(2).geometryFile = 'geometry/Spar.stl'; 
-body(2).mass = 20.2679;        % volume*rho            
-body(2).inertia = [3.0842 3.0842 0.6281];
-body(2).centerGravity = [0 0 -1.2477];
+body(2).geometryFile = 'geometry/spar.stl'; 
+body(2).mass = 17.1497;        % volume*rho            
+body(2).inertia = [ 17.1497*7^2  17.1497*7^2 0.3455];
+body(2).centerGravity = [0 0 -7];
 body(2).volume = 0.0156;
-body(2).initial.displacement = [0 0 0];
+body(2).initial.displacement = [0 0 1.4];
 body(2).nonlinearHydro = 2;
 
 if (body(1).mass + body(2).mass) > (body(1).volume + body(2).volume)*simu.rho
@@ -102,22 +102,28 @@ end
 constraint(1) = constraintClass('Constraint1'); % Initialize Constraint Class for Constraint1
 constraint(1).location = [0 0 0];               % Constraint Location [m]
 constraint(1).hardStops.upperLimitSpecify = 'off';
+constraint(1).hardStops.upperLimitBound = 0.2;
+constraint(1).hardStops.upperLimitDamping = 100;
+constraint(1).hardStops.upperLimitStiffness = 100;
 constraint(1).hardStops.lowerLimitSpecify = 'off';
+constraint(1).hardStops.lowerLimitBound = -0.2;
+constraint(1).hardStops.lowerLimitDamping = 100;
+constraint(1).hardStops.lowerLimitStiffness = 100;
 
 % Translational PTO
 pto(1) = ptoClass('PTO1');                      % Initialize PTO Class for PTO1
-pto(1).stiffness = 0;                           %PTO Stiffness [N/m]
-pto(1).damping = 100;                          % PTO Damping [N/(m/s)]
+pto(1).stiffness = 0;                           % PTO Stiffness [N/m]
+pto(1).damping = 200;                           % PTO Damping [N/(m/s)]
 pto(1).location = [0 0 0];                      % PTO Location [m]
 pto(1).equilibriumPosition = 0;                 % PTO Equilibrium Position [m]
 pto(1).hardStops.upperLimitSpecify = 'on';
-pto(1).hardStops.upperLimitBound = 0.2;
-pto(1).hardStops.upperLimitDamping = 10000;
-pto(1).hardStops.upperLimitStiffness = 50000;
+pto(1).hardStops.upperLimitBound = 0.1;
+pto(1).hardStops.upperLimitDamping = 0;
+pto(1).hardStops.upperLimitStiffness = 500;
 pto(1).hardStops.lowerLimitSpecify = 'on';
-pto(1).hardStops.lowerLimitBound = -0.2;
-pto(1).hardStops.lowerLimitDamping = 10000;
-pto(1).hardStops.lowerLimitStiffness = 50000;
+pto(1).hardStops.lowerLimitBound = -0.1;
+pto(1).hardStops.lowerLimitDamping = 0;
+pto(1).hardStops.lowerLimitStiffness = 500;
 
 %% Mooring constrain
 mooring(1) = mooringClass('mooring1');
